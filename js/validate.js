c07a84a6fe9b99be022a7ac3bc5891aeebab56dc
karam084 <karam.ezz84@gmail.com>
2022-01-28 04:35:05 +0200

    Add validation for email and message

--- js/validate.js
+++ js/validate.js
@@ -1,19 +1,66 @@
-const emailvalid = (email) => {
-  const emailRegex = '^([a-zA-Z0-9_-.]+)@([a-zA-Z0-9_-.]+).([a-zA-Z]{2,5})$';
+const isEmailValid = (email) => {
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailRegex.test(String(email).toLowerCase());
 };
 
 const form = document.querySelector('form');
-const nameInput = document.querySelector('input[name ="user_name"]');
-const emailInput = document.querySelector('email');
+const nameInput = document.querySelector('input[name="user_name"]');
+const emailInput = document.querySelector('input[name="user_email"]');
+const messageInput = document.querySelector('textarea[name="user_message"]')
+const inputs= {
+  nameInput,
+  emailInput,
+  messageInput,
+}
+
+let formValid = false;
+let isvalid = false;
+
+let resetData = (element) => {
+  element.classList.remove("invalid");
+  element.nextElementSibling.classList.add('hidden');
+};
+
+const invalidElement = (element) =>{
+  element.classList.add('invalid');
+  element.nextElementSibling.classList.remove('hidden');
+}
 
 const validInput = () => {
+  if(!isvalid) return;
+  formValid = true;
+  resetData(nameInput); 
+  resetData(emailInput);
+  resetData(messageInput)
+
   if (!nameInput.value) {
-    nameInput.classList.add('invalid');
-    nameInput.nextElementSibling.classList.remove('hidden');
+      formValid = false;
+      invalidElement(nameInput);
+  }
+
+  if (!isEmailValid(emailInput.value)) {
+    formValid = false;
+    invalidElement(emailInput);
+  }
+
+  if(!messageInput.value){
+    formValid = false;
+    invalidElement(messageInput);
   }
 };
 
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  isvalid = true;
+  validInput();
+  
+
+});
+
+inputs.forEach("input", ()=>{
+  input.addEventListener("input",()=>{
+    validInput();
+  
+  });
 });
