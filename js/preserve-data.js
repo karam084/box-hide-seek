c25caf3f62c5a3ef63472658d4a169495a428917
nstrvs <hi@nestorvalles.com>
2022-01-28 15:40:44 -0700

    Add JavaScrypt conditions

--- js/preserve-data.js
+++ js/preserve-data.js
@@ -26,13 +26,14 @@ const getFormData = () => {
   return data;
 };
 
-
-
-
-
-
-
-
-
-
-
+const populateForm = () => {
+  if (localStorage.key(formIdentifier)) {
+  const savedData = JSON.parse(localStorage.getItem(formIdentifier)); 
+  for (const element of formElements) {
+  if (element.name in savedData) {
+  element.value = savedData[element.name];
+  }
+  }
+  }
+  };
+  document.onload = populateForm(); 
