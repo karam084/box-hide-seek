ea0c4a306b1180a127ef62a5a5308b76e0c4665c
karam084 <karam.ezz84@gmail.com>
2022-01-28 07:29:56 +0200

    fix linter errors

--- js/validate.js
+++ js/validate.js
@@ -1,41 +1,41 @@
 const isEmailValid = (email) => {
-  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailRegex.test(String(email).toLowerCase());
 };
 
 const form = document.querySelector('form');
 const nameInput = document.querySelector('input[name="user_name"]');
 const emailInput = document.querySelector('input[name="user_email"]');
-const messageInput = document.querySelector('textarea[name="user_message"]')
-const inputs= {
+const messageInput = document.querySelector('textarea[name="user_message"]');
+const inputs = {
   nameInput,
   emailInput,
   messageInput,
-}
+};
 
 let formValid = false;
 let isvalid = false;
 
-let resetData = (element) => {
-  element.classList.remove("invalid");
+const resetData = (element) => {
+  element.classList.remove('invalid');
   element.nextElementSibling.classList.add('hidden');
 };
 
-const invalidElement = (element) =>{
+const invalidElement = (element) => {
   element.classList.add('invalid');
   element.nextElementSibling.classList.remove('hidden');
-}
+};
 
 const validInput = () => {
-  if(!isvalid) return;
-  formValid = true;
-  resetData(nameInput); 
+  if (!isvalid) return;
+  if (!formValid) return;
+  resetData(nameInput);
   resetData(emailInput);
-  resetData(messageInput)
+  resetData(messageInput);
 
   if (!nameInput.value) {
-      formValid = false;
-      invalidElement(nameInput);
+    formValid = false;
+    invalidElement(nameInput);
   }
 
   if (!isEmailValid(emailInput.value)) {
@@ -43,24 +43,21 @@ const validInput = () => {
     invalidElement(emailInput);
   }
 
-  if(!messageInput.value){
+  if (!messageInput.value) {
     formValid = false;
     invalidElement(messageInput);
   }
 };
 
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   isvalid = true;
+  formValid = true;
   validInput();
-  
-
 });
 
-inputs.forEach("input", ()=>{
-  input.addEventListener("input",()=>{
+inputs.forEach('input', () => {
+  inputs.addEventListener('input', () => {
     validInput();
-  
   });
 });
